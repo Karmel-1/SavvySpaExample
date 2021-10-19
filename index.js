f6282fb3f1e70c1a4ee791838b301a95f6f9078a
Karmen Beers <Karmenbeers@gmail.com>
2021-10-19 18:22:54 -0500

    pushing

--- index.js
+++ index.js
@@ -1,18 +1,16 @@
+// 1. Import Statement
 import { Header, Nav, Main, Footer } from "./components";
 import * as state from "./store";
-
+import axios from "axios";
 import Navigo from "navigo";
 import { capitalize } from "lodash";
+import dotenv from "dotenv";
+dotenv.config();
 
+// 2. Declaring Router
 const router = new Navigo(window.location.origin);
 
-router
-  .on({
-    ":page": (params) => render(state[capitalize(params.page)]),
-    "/": () => render(state.Home),
-  })
-  .resolve();
-
+// 3. Render Function
 function render(st = state.Home) {
   document.querySelector("#root").innerHTML = `
   ${Header(st)}
@@ -26,10 +24,11 @@ function render(st = state.Home) {
   addEventListeners(st);
 }
 
+// 4. Event Handler Function
 function addEventListeners(st) {
   // add event listeners to Nav items for navigation
-  document.querySelectorAll("nav a").forEach((navLink) =>
-    navLink.addEventListener("click", (event) => {
+  document.querySelectorAll("nav a").forEach(navLink =>
+    navLink.addEventListener("click", event => {
       event.preventDefault();
       render(state[event.target.title]);
     })
@@ -44,7 +43,7 @@ function addEventListeners(st) {
 
   // event listener for the the photo form
   if (st.view === "Form") {
-    document.querySelector("form").addEventListener("submit", (event) => {
+    document.querySelector("form").addEventListener("submit", event => {
       event.preventDefault();
       // convert HTML elements to Array
       let inputList = Array.from(event.target.elements);
@@ -61,3 +60,47 @@ function addEventListeners(st) {
     });
   }
 }
+
+// 5. Router.hooks
+router.hooks({
+  before: (done, params) => {
+    const page =
+      params && params.hasOwnProperty("page")
+        ? capitalize(params.page)
+        : "Home";
+    if (page === "Home") {
+      axios
+        .get(
+          `https://api.openweathermap.org/data/2.5/weather?appid=${process.env.WEATHER_API_KEY}&q=st.%20louis`
+        )
+        .then(response => {
+          state.Home.weather = {};
+          state.Home.weather.city = response.data.name;
+          state.Home.weather.temp = response.data.main.temp;
+          state.Home.weather.feelsLike = response.data.main.feels_like;
+          state.Home.weather.description = response.data.weather[0].main;
+          done();
+        })
+        .catch(err => console.log(err));
+    }
+    if (page === "Pizza") {
+      axios
+        .get(`${process.env.PIZZA_PLACE_API_URL}`)
+        .then(response => {
+          state.Pizza.pizzas = response.data;
+          done();
+        })
+        .catch(error => {
+          console.log("It puked", error);
+        });
+    }
+  }
+});
+
+// 6. Router.on
+router
+  .on({
+    "/": () => render(state.Home),
+    ":page": params => render(state[capitalize(params.page)])
+  })
+  .resolve();
---
+++ server/routes/pizzas.js
@@ -0,0 +1,14 @@
+const { Router } = require("express");
+const pizza = require("../models/Pizza");
+const router = Router();
+router.route("/").get((request, response) => {
+  // handle GET request
+  response.send(JSON.stringify({ message: "All pizzas" }));
+});
+router.post("/", (request, response) => {
+  const newPizza = new pizza.model(request.body);
+  newPizza.save((err, pizza) => {
+    return err ? response.sendStatus(500).json(err) : response.json(pizza);
+  });
+});
+module.exports = router;
