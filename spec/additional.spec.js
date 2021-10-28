6c292bc75397d3f89b58f8af4ead638e9ab5e886
Karmen Beers <Karmenbeers@gmail.com>
2021-10-28 16:47:47 -0500

    updating

---
+++ spec/additional.spec.js
@@ -0,0 +1,17 @@
+const assert = require("assert");
+const addition = require("../addition");
+
+describe("addition()", function() {
+  //arrange
+  let a = 2;
+  let b = 2;
+  let sum = 4;
+
+  //act
+  let result = addition(a, b);
+
+  //assert
+  it("it should return 4", function() {
+    expect(result).toEqual(sum);
+  });
+});
